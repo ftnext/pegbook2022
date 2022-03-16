713855281894c84006ab1f0de49b8c97fda6602a
ftnext <takuyafjp+develop@gmail.com>
2022-03-16 22:05:09 +0900

    [test] Add 2 terms

--- introduction/test_first_parser.py
+++ introduction/test_first_parser.py
@@ -6,3 +6,6 @@ from first_parser import calc
 class CalcTestCase(TestCase):
     def test_case1_str_to_float(self):
         self.assertEqual(calc("1"), 1.0)
+
+    def test_case2_add_two_terms(self):
+        self.assertEqual(calc("1+2"), 3.0)
