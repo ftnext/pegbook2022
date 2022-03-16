9ad6720d56ce770d14244cf03fff344903c87372
ftnext <takuyafjp+develop@gmail.com>
2022-03-16 22:21:35 +0900

    [feat] Parse "+" (only 2 terms)

--- introduction/first_parser.py
+++ introduction/first_parser.py
@@ -1,2 +1,9 @@
 def calc(s):
+    pos = s.find("+")
+    # 見つからなければ-1、先頭で見つかる(0)はありえない
+    # ref: https://docs.python.org/ja/3/library/stdtypes.html#str.find
+    if pos > 0:
+        left = s[:pos]
+        right = s[pos + 1 :]
+        return float(left) + float(right)
     return float(s)
