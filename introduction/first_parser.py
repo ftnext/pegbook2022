dbdd7e0e4cfb46656765c6fc7040fae1bd516131
ftnext <takuyafjp+develop@gmail.com>
2022-03-16 22:49:17 +0900

    [feat] Implement multiply, be aware of order

--- introduction/first_parser.py
+++ introduction/first_parser.py
@@ -1,4 +1,5 @@
 def calc(s):
+    # +と*では*が先に計算される（*について先に再帰でcalcを呼び出す）ように、+の処理が先
     pos = s.find("+")
     # 見つからなければ-1、先頭で見つかる(0)はありえない
     # ref: https://docs.python.org/ja/3/library/stdtypes.html#str.find
@@ -6,4 +7,10 @@ def calc(s):
         left = s[:pos]
         right = s[pos + 1 :]
         return calc(left) + calc(right)
+
+    pos = s.find("*")
+    if pos > 0:
+        left = s[:pos]
+        right = s[pos + 1 :]
+        return calc(left) * calc(right)
     return float(s)
